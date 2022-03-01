75f58c4a80b0ef217bdbd7bfb9d600b53c55a819
Noé Cuyuch <58575608+wasdNoah@users.noreply.github.com>
2022-03-01 14:31:37 -0600

    Added Task
    Added task view and template, no styling tho

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, Task
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', Task.as_view(), name='task'),
 ]
