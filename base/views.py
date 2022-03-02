1aa9c395f17be01adc0677bbea77ff97ba6da9a8
Noé Cuyuch <58575608+wasdNoah@users.noreply.github.com>
2022-03-01 20:52:14 -0600

    Added Update and Delete
    Update and Delete views and templates

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from base.models import Task
@@ -17,4 +17,14 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
