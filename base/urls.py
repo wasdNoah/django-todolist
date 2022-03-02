a0d14ea206827f307f96d2298125c54f2455106b
Noé Cuyuch <58575608+wasdNoah@users.noreply.github.com>
2022-03-01 20:37:45 -0600

    Create task
    Added the view and template to create a Task. No styling yet

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, Task
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>', Task.as_view(), name='task'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from base.models import Task
 
@@ -8,7 +9,12 @@ class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
-class Task(DetailView):
+class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
